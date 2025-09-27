310c8fe029e18a59d5f65ffbfe887cc74a713449
ggtkh <pashaazarov2008@gmail.com>
2025-09-27 23:14:38 +0300

    я заколебался

--- base/settings.py
+++ base/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-0p1m-)*+gsh+bv0o!!m*t$ax&vk&g&(_x_^p!y%y0u7v+23_pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fgt.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
